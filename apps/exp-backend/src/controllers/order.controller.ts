fe6a4970a5c5b7f14b7f1cc3e111ecc90a958dfa
Aditya @ArchLinux <132184385+adityadeshlahre@users.noreply.github.com>
2023-11-26 23:24:48 +0530

    order function fixing

--- apps/exp-backend/src/controllers/order.controller.ts
+++ apps/exp-backend/src/controllers/order.controller.ts
@@ -1,39 +1,41 @@
 //need fix
+//null values fix
+//product values fetching using productId
+//CartItems aren't getting empty
+//orderIs not being used as queue
 
 import { Request, Response } from "express";
 import { PrismaClient } from "../../../../prisma/generated/client";
 
 const prisma = new PrismaClient();
 
-const fetchProductsForSpecific = async (userId: number) => {
+const fetchProductsForSpecific = async (userId: number, isAdmin: boolean) => {
   try {
-    const userWithProducts = await prisma.user.findUnique({
-      where: { id: userId },
-      include: {
-        cartItems: {
+    const result = isAdmin
+      ? await prisma.admin.findUnique({
+          where: { id: userId },
           include: {
-            product: true,
+            cartItems: {
+              include: {
+                product: true,
+              },
+            },
           },
-        },
-      },
-    });
-
-    const adminWithProducts = await prisma.admin.findUnique({
-      where: { id: userId },
-      include: {
-        cartItems: {
+        })
+      : await prisma.user.findUnique({
+          where: { id: userId },
           include: {
-            product: true,
+            cartItems: {
+              include: {
+                product: true,
+              },
+            },
           },
-        },
-      },
-    });
+        });
 
-    if (userWithProducts || adminWithProducts) {
-      const cartItems =
-        (userWithProducts || adminWithProducts)?.cartItems || [];
+    if (result) {
+      const cartItems = result.cartItems || [];
       const products = cartItems.map((cartItem) => cartItem.product);
-      console.log(products);
       return products;
     } else {
       console.log("User or admin not found");
@@ -48,86 +50,97 @@ const fetchProductsForSpecific = async (userId: number) => {
 export const order = async (req: Request, res: Response) => {
   try {
     const userId: number | undefined = req.userId;
-    console.log(userId);
+    const isAdmin: boolean = req.isAdmin || false;
+
     if (userId === undefined) {
       return res
         .status(400)
         .json({ error: "Invalid request, userId is missing" });
     }
-    const products = await fetchProductsForSpecific(userId);
-    console.log(products);
+
+    const products = await fetchProductsForSpecific(userId, isAdmin);
 
     const { productId, status } = req.body;
 
-    const orderStatus = status === "yes" ? "completed" : "pending";
+    const orderStatus = status === "1" ? "completed" : "pending";
 
-    const order = await prisma.order.create({
-      data: {
-        userId,
-        adminId: null,
-        productId,
-        status: orderStatus,
-        total: 0,
-      },
-    });
+    // If status is "1", move items to order history
+    if (status === "1" && products.length > 0) {
+      const total = products.reduce((acc, product) => acc + product.price, 0);
 
-    if (status === "yes") {
-      const orderHistory = await prisma.orderHistory.create({
+      // Create a new order
+      let order;
+      if (isAdmin) {
+        order = await prisma.order.create({
+          data: {
+            userId: null,
+            adminId: userId,
+            productId,
+            status: orderStatus,
+            total,
+          },
+        });
+      } else {
+        order = await prisma.order.create({
+          data: {
+            userId,
+            adminId: null,
+            productId,
+            status: orderStatus,
+            total,
+          },
+        });
+      }
+
+      // Create order history
+      await prisma.orderHistory.create({
         data: {
           userId: order.userId,
           adminId: order.adminId,
-          productId: order.productId,
+          productId,
           status: "completed",
           total: order.total,
         },
       });
 
-      await prisma.order.delete({
-        where: {
-          id: order.id,
-        },
+      // Clear the user's cart
+      await prisma.cartItem.updateMany({
+        where: { userId },
+        data: { quantity: 0 },
       });
+
+      res.json({ message: "Order placed successfully" });
+    } else {
+      res.json({ message: "Order wans't successfully placed" });
     }
-
-    await prisma.cartItem.updateMany({
-      where: { userId },
-      data: { quantity: 0 },
-    });
-
-    res.json(products);
   } catch (error) {
-    console.error("Error creating order:", error);
+    console.error("Error processing order:", error);
     res.status(500).json({ error: "Internal server error" });
   }
 };
 
 export const orderHistory = async (req: Request, res: Response) => {
   try {
-    const { orderId } = req.body;
-    const order = await prisma.order.findUnique({
+    const userId: number | undefined = req.userId;
+    const isAdmin: boolean = req.isAdmin || false;
+
+    if (userId === undefined) {
+      return res
+        .status(400)
+        .json({ error: "Invalid request, userId is missing" });
+    }
+
+    // Retrieve order history for the user or admin
+    const orderHistory = await prisma.orderHistory.findMany({
       where: {
-        id: orderId,
-        status: "completed",
+        userId: isAdmin ? null : userId,
+        adminId: isAdmin ? userId : null,
       },
     });
 
-    if (order) {
-      const orderHistory = await prisma.orderHistory.create({
-        data: {
-          userId: order.userId,
-          adminId: order.adminId,
-          productId: order.productId,
-          status: "completed",
-          total: order.total,
-        },
-      });
-
-      res.json(orderHistory);
-    } else {
-      res.status(400).json({ error: "Order not found or not completed" });
-    }
+    res.json(orderHistory);
   } catch (error) {
-    console.error("Error creating order history:", error);
+    console.error("Error retrieving order history:", error);
     res.status(500).json({ error: "Internal server error" });
   }
 };
--- apps/exp-backend/src/routes/order.routes.ts
+++ apps/exp-backend/src/routes/order.routes.ts
@@ -3,7 +3,7 @@ import express from "express";
 import { order, orderHistory } from "../controllers/order.controller";
 import { authenticateUser } from "../middleware/auth.middleware";
 const router = express.Router();
-router.get("/*", authenticateUser, order);
+router.get("/", authenticateUser, order);
 router.get("/history", authenticateUser, orderHistory);
 
 export default router;
